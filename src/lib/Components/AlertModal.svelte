d5b7cc4b5b6aca7b92baa3930ba1cbf1060e055d
macsakarn <macsakarn@gmail.com>
2023-01-25 23:46:21 +0700

    [➕] add Alert system

---
+++ src/lib/Components/AlertModal.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Alert } from 'flowbite-svelte';
+	import { flip } from 'svelte/animate';
+	import { fly } from 'svelte/transition';
+	import { notifications } from '$lib/notifications.js';
+	export let themes = {
+		danger: 'red',
+		success: 'green',
+		warning: 'yellow',
+		info: 'blue'
+	};
+</script>
+
+{#each $notifications as notification (notification.id)}
+	<div animate:flip transition:fly={{ y: 30 }}>
+		<Alert class="my-2" color={themes[notification.type]}>
+			<span class="font-medium">Info asddds!</span>
+			{notification.message}
+		</Alert>
+	</div>
+{/each}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
-<script>
+<script lang="ts">
 	import '../app.postcss';
-	import Navbar from "$lib/components/Navbar.svelte"; 
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Alert from '$lib/Components/AlertModal.svelte';
 </script>
 
 <div class="container relative">
-    <Navbar/>
+	<Navbar />
 </div>
 <div class="pt-20">
 	<slot />
+</div>
+
+<div class="alert">
+	<Alert />
 </div>
