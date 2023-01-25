3f2b1e2bf5d82916c82ae20eeef3c33b5b9d26ce
macsakarn <macsakarn@gmail.com>
2023-01-25 20:42:38 +0700

    [➕] add component Nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.postcss';
+	import Navbar from "$lib/components/Navbar.svelte"; 
 </script>
 
-<slot />
+<div class="container relative">
+    <Navbar/>
+</div>
+<div class="pt-20">
+	<slot />
+</div>
