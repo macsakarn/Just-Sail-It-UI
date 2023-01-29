bb739e13549c6592a76aa262a3e18ddc838e41d4
macsakarn <macsakarn@gmail.com>
2023-01-29 16:02:17 +0700

    [🔃] add mode to fetch lib

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,7 +1,14 @@
 import { PUBLIC_API_URL } from '$env/static/public';
 
-async function fetchApi(method: 'GET' | 'POST' | 'PUT' | 'DELETE', url: string, body: any) {
-	return await fetch(`${PUBLIC_API_URL}${url}`, {
+async function fetchApi(
+	method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+	url: string,
+	body: any,
+	mode: 'server' | 'api'
+) {
+	const BASE_URL = mode === 'api' ? PUBLIC_API_URL : '';
+
+	return await fetch(`${BASE_URL}${url}`, {
 		method,
 		headers: {
 			'Content-Type': 'application/json'
