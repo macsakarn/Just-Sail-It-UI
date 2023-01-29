9bbb6821ae2e9fcf6fcabc0d3563f2ae66772c57
macsakarn <macsakarn@gmail.com>
2023-01-29 16:01:57 +0700

    [➕] add config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,35 @@
 const config = {
-  content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
-  ],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
 
-  theme: {
-    extend: {
-      colors:{
-        'primary': { "50": "#6667AB", "100": "#6667AB", "200": "#6667AB", "300": "#6667AB", "400": "#6667AB", "500": "#6667AB", "600": "#6667AB", "700": "#6667AB", "800": "#6667AB", "900": "#6667AB" },
-        'second':"#9C9AE3",
-        'web-white':"#F9F8FF",
-        'web-black':"#464555",
-        'web-gray':"#EBEBEB",
-        'dark-gray':"#ABA9BB",
-      }
-    },
-  },
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					50: '#6667AB',
+					100: '#6667AB',
+					200: '#6667AB',
+					300: '#6667AB',
+					400: '#6667AB',
+					500: '#6667AB',
+					600: '#6667AB',
+					700: '#6667AB',
+					800: '#6667AB',
+					900: '#6667AB'
+				},
+				second: '#9C9AE3',
+				'web-white': '#F9F8FF',
+				'web-black': '#464555',
+				'web-gray': '#EBEBEB',
+				'dark-gray': '#ABA9BB'
+			}
+		}
+	},
 
-  plugins: [
-    require('flowbite/plugin')
-  ],
-  darkMode: 'class',
+	plugins: [require('flowbite/plugin')],
+	darkMode: 'class'
 };
 
 module.exports = config;
