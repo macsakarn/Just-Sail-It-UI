eea61f1942b5445da5c0481df4975ab8ba7ef3b6
macsakarn <macsakarn@gmail.com>
2023-02-08 21:26:31 +0700

    [➕] upload image

--- src/routes/profile/+layout.svelte
+++ src/routes/profile/+layout.svelte
@@ -1,16 +1,50 @@
 <script lang="ts">
-	import { Avatar, P, A, Tabs, TabItem } from 'flowbite-svelte'
+	import { Avatar, P, A, Tabs, TabItem, Dropdown, DropdownItem, Label } from 'flowbite-svelte'
 	import { page } from '$app/stores'
 	$: path = $page.url.pathname
 	$: user = $page.data.user
+
+	function uploadProfile(event: Event) {
+		const target = event.target as HTMLSelectElement
+		target.form?.submit()
+	}
 </script>
 
 <div
-	class="h-60  bg-cover bg-no-repeat bg-center mb-28"
+	class="h-72  bg-cover bg-no-repeat bg-center mb-28"
 	style="background-image: url({user.image_cover});"
 >
-	<div class="relative -bottom-36 flex flex-col items-center h-auto gap-2">
-		<Avatar src={user.image} size="xl" border class="ring-web-white" />
+	<div class="relative -bottom-48 flex flex-col items-center h-auto gap-2">
+		<Avatar src={user.image} id="placements" size="xl" border class="ring-web-white" />
+		<Dropdown placement="right" triggeredBy="#placements">
+			<form action="/api/profile/uploadProfile" method="POST" enctype="multipart/form-data">
+				<DropdownItem type="button">
+					<Label for="avatar">Upload Profile</Label>
+					<input
+						type="file"
+						id="avatar"
+						name="profile_image"
+						accept="image/png, image/jpeg"
+						on:change={uploadProfile}
+						hidden
+					/>
+				</DropdownItem>
+			</form>
+			<form action="/api/profile/uploadCover" method="POST" enctype="multipart/form-data">
+				<DropdownItem type="button">
+					<Label for="cover">Upload Cover</Label>
+					<input
+						type="file"
+						id="cover"
+						name="cover_image"
+						accept="image/png, image/jpeg"
+						on:change={uploadProfile}
+						hidden
+					/>
+				</DropdownItem>
+			</form>
+				
+		</Dropdown>
 		<A aClass="text-xs">@{user.userId}</A>
 		<P size="2xl">{user.username}</P>
 	</div>
--- src/routes/profile/history/+page.svelte
+++ src/routes/profile/history/+page.svelte
@@ -1,4 +1,38 @@
 <script lang="ts">
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte'
 </script>
 
-this is history page
+<div class="container">
+	<Table>
+		<TableHead>
+			<TableHeadCell>Date</TableHeadCell>
+			<TableHeadCell>Project Name</TableHeadCell>
+			<TableHeadCell>Donate Amount</TableHeadCell>
+	
+		</TableHead>
+		<TableBody >
+			<TableBodyRow>
+				<TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
+				<TableBodyCell>Sliver</TableBodyCell>
+				<TableBodyCell>Laptop</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow>
+				<TableBodyCell>Microsoft Surface Pro</TableBodyCell>
+				<TableBodyCell>White</TableBodyCell>
+				<TableBodyCell>Laptop PC</TableBodyCell>
+			</TableBodyRow>
+			<TableBodyRow>
+				<TableBodyCell>Magic Mouse 2</TableBodyCell>
+				<TableBodyCell>Black</TableBodyCell>
+				<TableBodyCell>Accessories</TableBodyCell>
+			</TableBodyRow>
+		</TableBody>
+	</Table>
+</div>
